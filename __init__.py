54f14113cdeeb260a1cd78180f086138426370c9
jordan wakelam <jwakelams@gmail.com>
2025-01-17 14:34:28 +0100

    exo jusqu'à 3Bis ok

--- __init__.py
+++ __init__.py
@@ -4,12 +4,46 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
-  
+
+
+@app.route("/contact/")
+def contact():
+    return "<h2>Ma page contact</h2>"
+
+
+@app.route("/tawano/")
+def meteo():
+    response = urlopen(
+        "https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx"
+    )
+
+    raw_content = response.read()
+
+    json_content = json.loads(raw_content.decode("utf-8"))
+
+    results = []
+
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+
+        temp_day_value = list_element.get('main', {}).get(
+            'temp') - 273.15  # voir pourquoi çà permet de convertir en °c
+
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
+
+@app.route("/rapport/")
+def monGraphique():
+    return render_template("graphique.html")
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
